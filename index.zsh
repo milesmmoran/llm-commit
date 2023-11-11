70d7a4f72b2c5419420bf4eee7833dc3fe63ce44
Miles <miles@karmacheck.com>
2023-11-11 03:05:06 -0600

    Refactored the script to generate and commit changes asynchronously in the background.

--- index.zsh
+++ index.zsh
@@ -15,21 +15,21 @@ is_subdir() {
     fi
 }
 
-# Function to generate commit message
-generate_commit_message() {
+# Function to generate and perform commit asynchronously
+generate_and_commit() {
     # Capture git diff of staged files
     git_diff=$(git diff --cached)
 
     # If there are no staged changes, exit
     if [ -z "$git_diff" ]; then
         echo "No staged changes to commit."
-        exit 0
+        return
     fi
 
     # Prepare the payload with the git diff
     payload=$(jq -n \
                   --arg diff "$git_diff" \
-                  '{model: "gpt-3.5-turbo", messages: [{role: "system", content: "You are an assistant who generates commit messages based on git diffs of staged changes. "}, {role: "user", content: $diff}]}')
+                  '{model: "gpt-3.5-turbo", messages: [{role: "system", content: "You are an assistant who generates commit messages based on git diffs of staged changes."}, {role: "user", content: $diff}]}')
 
     # Call the OpenAI API
     response=$(curl -s https://api.openai.com/v1/chat/completions \
@@ -40,8 +40,11 @@ generate_commit_message() {
     # Extract the commit message from the response
     commit_message=$(echo "$response" | jq -r '.choices[0].message.content')
 
-    # Return the commit message
-    echo "$commit_message"
+    # Commit the changes with the generated message
+    if [ -n "$commit_message" ]; then
+        git commit -m "$commit_message"
+        echo "Committed with AI-generated message: $commit_message"
+    fi
 }
 
 # Main script execution
@@ -50,12 +53,6 @@ current_path=$(git rev-parse --show-toplevel)
 
 # Check if the current path is not a subdirectory of the Projects directory
 if ! is_subdir $work_path $current_path; then
-    # Generate a commit message for staged changes
-    commit_msg=$(generate_commit_message)
-
-    # If a commit message was generated, use it to commit the changes
-    if [ -n "$commit_msg" ]; then
-        git commit -m "$commit_msg"
-        echo "Committed with AI-generated message: $commit_msg"
-    fi
+    # Call generate_and_commit in the background
+    generate_and_commit &
 fi
